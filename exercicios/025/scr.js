7b2ff128620df898d6f4fd5fb044b65c51ddafa4
Matheus Vidal <matheusvidal305@gmail.com>
2023-02-25 10:40:24 -0300

    Atualização de exercícios

---
+++ exercicios/025/scr.js
@@ -0,0 +1,7 @@
+function mi(){
+    idade.innerHTML = '<p><input type="button" value="Tempo de Vida" onclick="tv()"> <input type="button" value="Meses de Vida"> <input type="button" value="Dias de Vida"> <input type="button" value="Horas de Vida"> <input type="button" value="Minutos de Vida"></p>'
+}
+
+function tv(){
+    document.write("flbkobmsofdm")
+}
